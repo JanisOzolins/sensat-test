9ba22b01d9992cf294cdbe07d783aa20d25fbbca
JanisOzolins <janis@janisozolins.com>
2021-02-28 18:42:19 +0000

    Read JSON file lines and cast the result into an array of SensorData objects - as the provided JSON file is invalid and cannot be read directly - it needs to be read line-by-line as strings - then checked for validity and converted into SensorData objects

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataTableComponent } from './components/data-table/data-table.component';
@@ -14,7 +14,8 @@ import { DashboardContainer } from './containers/dashboard/dashboard.container';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- app/src/app/containers/dashboard/dashboard.container.ts
+++ app/src/app/containers/dashboard/dashboard.container.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { SensorData } from 'src/app/model/sensor-data.model';
 
 @Component({
   selector: 'app-dashboard-container',
@@ -6,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardContainer implements OnInit {
 
-  constructor() { }
+  sensorData: SensorData[] = [];
+  fileReader = new FileReader();
+  lineNumber = 0;
+
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit(): void {
+    this.http.get('/assets/sensor_readings.json', { responseType: 'blob' })
+      .subscribe(res => {
+        this.fileReader.onloadend = () => {
+          const temp = this.fileReader.result as string;
+          const stringArray = temp?.split('\n');
+          this.sensorData = stringArray.filter(line => this.isLineValid(line)).map( line => {
+            const parsedLine = JSON.parse(line);
+            return parsedLine as SensorData;
+          })
+        }
+
+        this.fileReader.readAsText(res); 
+      });
+  }
+
+  isLineValid(line: string): boolean {
+    if("" === line) return false;
+
+    try {
+        JSON.parse(line);
+        this.lineNumber++;
+    } catch (e) {
+        console.log("Couldn't parse line:", this.lineNumber);
+        return false;
+    }
+    return true;
   }
 
 }
