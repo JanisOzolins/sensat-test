2efd205a1d0fc37f85cf13edad09e7bdbea72142
JanisOzolins <janis@janisozolins.com>
2021-02-28 16:45:00 +0000

    Set up '/dashboard' as the main route

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardContainer } from './containers/dashboard/dashboard.container';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:  'dashboard', component:  DashboardContainer },
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
